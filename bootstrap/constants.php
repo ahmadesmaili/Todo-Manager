465e471b9abaaac54fdd646afbcaff1415d48040
Ahmad Esmaili <ahmadesmaili71@gmail.com>
2022-11-25 04:17:01 +0330

    remove .idea

--- bootstrap/constants.php
+++ bootstrap/constants.php
@@ -2,5 +2,5 @@
 
 define('SITE_TITLE', '7Task Project');
 $protocol=$_SERVER['PROTOCOL'] = isset($_SERVER['HTTPS']) && !empty($_SERVER['HTTPS']) ? 'https' : 'http';
-define('BASE_URL', "{$protocol}://{$_SERVER['HTTP_HOST']}/");
+define('BASE_URL', "$protocol://{$_SERVER['HTTP_HOST']}/");
 define('BASE_PATH', "{$_SERVER['DOCUMENT_ROOT']}/");
